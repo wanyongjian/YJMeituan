ac9eeb7acc8ffee213f818a594799007865014e9
yongjian.wan <yongjian.wan@ecovacs.com>
2017-07-26 15:16:54 +0800

    工程提交

---
+++ YJMeituan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJMeituan
+//
+//  Created by wanyongjian on 2017/7/26.
+//  Copyright © 2017年 eco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YJMeituan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJMeituan
+//
+//  Created by wanyongjian on 2017/7/26.
+//  Copyright © 2017年 eco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
